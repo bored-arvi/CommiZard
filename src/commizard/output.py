40b5f9dd0efaf5ff7ace0ec817b258095f5346bb
Chungzter <freezycry@protonmail.com>
2025-09-09 03:11:48 +0330

    add output.py: formatting output
    give accessibility

---
+++ src/commizard/output.py
@@ -0,0 +1,57 @@
+from rich.console import Console
+
+console = Console()
+
+
+def print_success(message: str) -> None:
+    """
+    prints success message in green color
+
+    Args:
+        message: success message to print
+
+    Returns:
+        None
+    """
+    console.print(f"[green]{message}[/green]")
+
+
+def print_error(message: str) -> None:
+    """
+    prints error message in red color
+
+    Args:
+        message: error message to print
+
+    Returns:
+        None
+    """
+    console.print(f"[red]Error: {message}[/red]")
+
+
+def print_warning(message: str) -> None:
+    """
+    prints warning message in yellow color
+
+    Args:
+        message: warning message to print
+
+    Returns:
+        None
+    """
+    console.print(f"[yellow]Warning: {message}[/yellow]")
+
+
+def print_generated(message: str) -> None:
+    """
+    prints generated message in blue color
+
+    Args:
+        message: generated message to print
+
+    Returns:
+        None
+    """
+    console.print(f"[blue]{message}[/blue]")
+
+# TODO: add wrapping function for output
