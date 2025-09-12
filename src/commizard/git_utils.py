28bc933a7166e87584aeb98728b6e7af2721e022
Chungzter <freezycry@protonmail.com>
2025-09-12 13:25:06 +0330

    add git_utils.py
    this file will act as an api for interfacing with git.

---
+++ src/commizard/git_utils.py
@@ -0,0 +1,46 @@
+# TODO: use this module as a git API everywhere
+import subprocess
+
+
+def run_git_command(args: list[str]) -> subprocess.CompletedProcess:
+    """
+    Run a git command with the given args.
+
+    Returns:
+        a CompletedProcess object
+    """
+    cmd = ['git'] + args
+    return subprocess.run(cmd, capture_output=True, text=True)
+
+
+def is_inside_working_tree() -> bool:
+    """
+    Check if we're inside a working directory (can execute commit and diff
+    commands)
+    """
+    out = run_git_command(["rev-parse", "--is-inside-work-tree"])
+    return out.returncode == 0 and out.stdout.strip() == "true"
+
+
+def is_changed() -> bool:
+    """
+    Check if we have changed files
+    """
+    out = run_git_command(["diff", "--name-only", "--no-pager"])
+    return (out.returncode == 0) and (out.stdout.strip() != "")
+
+
+def get_diff() -> str:
+    """
+    Get the diff from the current working directory.
+
+    Returns:
+        the diff as a string
+    """
+    if not is_changed():
+        return ""
+
+    out = run_git_command(["--no-pager", "diff"])
+
+    if out.returncode == 0 and out.stderr.strip() == "":
+        return out.stdout.strip()
