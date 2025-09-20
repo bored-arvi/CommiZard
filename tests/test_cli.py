376be3f19422d3651d3c89e97808fa027de478a5
Chungzter <freezycry@protonmail.com>
2025-09-20 21:27:45 +0330

    add test_cli.py

---
+++ tests/test_cli.py
@@ -0,0 +1,70 @@
+from unittest.mock import patch
+
+import pytest
+from commizard import cli
+
+
+@pytest.mark.parametrize(
+    "git_installed, local_ai_avail, inside_work_tree, user_inputs, num_parse",
+    [
+        (False, True, True, [""], 0),
+        (True, True, False, [""], 0),
+        (True, True, True, ["exit"], 0),
+        (True, False, True, ["quit"], 0),
+        (True, True, True, ["cmd1", "cmd2", "cmd3"], 3),
+        (True, True, True, ["cmd" for cmd in range(40)], 40),
+    ],
+    ids=[
+        "git_not_installed",
+        "not_in_work_tree",
+        "happy_exit",
+        "ai_unavailable_quit",
+        "multiple_commands_then_exit",
+        "very_long_session",
+    ],
+)
+@patch("commizard.cli.start.check_git_installed")
+@patch("commizard.cli.start.local_ai_available")
+@patch("commizard.cli.start.is_inside_working_tree")
+@patch("commizard.cli.start.print_welcome")
+@patch("commizard.cli.commands.parser")
+@patch("builtins.input")
+@patch("commizard.cli.print_error")
+@patch("commizard.cli.print_warning")
+@patch("builtins.print")
+def test_main(mock_print, mock_warning, mock_error, mock_input, mock_parser,
+              mock_welcome, mock_is_inside_work_tree, mock_local_ai,
+              mock_check_git_installed, git_installed, local_ai_avail,
+              inside_work_tree, user_inputs, num_parse):
+    mock_check_git_installed.return_value = git_installed
+    mock_is_inside_work_tree.return_value = inside_work_tree
+    mock_local_ai.return_value = local_ai_avail
+    mock_input.side_effect = user_inputs + ["exit"]
+    cli.main()
+
+    mock_check_git_installed.assert_called_once()
+
+    if not git_installed:
+        mock_error.assert_called_once_with("git not installed")
+        mock_welcome.assert_not_called()
+        mock_parser.assert_not_called()
+        return
+    else:
+        mock_is_inside_work_tree.assert_called_once()
+
+    if not inside_work_tree:
+        mock_error.assert_called_once_with("not inside work tree")
+        mock_welcome.assert_not_called()
+        mock_parser.assert_not_called()
+        return
+
+    mock_welcome.assert_called_once()
+    # Now we're in the loop
+    if not local_ai_avail:
+        mock_warning.assert_called_once_with("local AI not available")
+
+    assert mock_parser.call_count == num_parse
+
+    # exit the loop
+    assert mock_print.call_count == 1
+    mock_print.assert_called_once_with("Goodbye!")
