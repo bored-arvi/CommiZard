bdee40e348794b430adc64e8abe04cbd938540b8
Chungzter <freezycry@protonmail.com>
2025-09-27 19:48:12 +0330

    cli.py: add handle_args()

--- src/commizard/cli.py
+++ src/commizard/cli.py
@@ -1,6 +1,18 @@
+import sys
+
+from commizard import __version__ as version
+
 from . import commands
 from . import start
 from .output import *
+
+
+def handle_args():
+    if len(sys.argv) < 2:
+        return
+    if sys.argv[1] in ("-v", "--version"):
+        print(f"CommiZard {version}")
+        sys.exit(0)
 
 
 # TODO: see issues #2 and #3
@@ -9,6 +21,7 @@ def main() -> None:
     This is the entry point of the program. calls some functions at the start,
     then jumps into an infinite loop.
     """
+    handle_args()
     if not start.check_git_installed():
         print_error("git not installed")
         return
