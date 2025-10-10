ea0e289ad688f650b1f94edb7b1e803dc14d0cd7
Chungzter <freezycry@protonmail.com>
2025-10-10 18:11:37 +0330

    fix encoding issues on testing

--- tests/e2e/test_user_workflow.py
+++ tests/e2e/test_user_workflow.py
@@ -4,6 +4,7 @@ working tree (in the git repo, not the .git folder), and the ollama server is
 correctly working and ready to generate a message.
 """
 
+import os
 import subprocess
 
 import pytest
@@ -18,12 +19,13 @@ import pytest
     ],
 )
 def test_early_exit(user_in):
+    env = {**os.environ, "PYTHONIOENCODING": "utf-8"}
     out = subprocess.run(
         ["commizard"],
         capture_output=True,
-        text=True,
-        input=user_in,
+        input=user_in.encode(),
         timeout=5,
+        env=env,
     )
     assert out.returncode == 0
-    assert out.stderr.strip() == ""
+    assert out.stderr.decode("utf-8").strip() == ""
