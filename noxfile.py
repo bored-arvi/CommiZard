027edb11a413cd3952d572211cfcfac4e65fdc04
Chungzter <freezycry@protonmail.com>
2025-10-10 23:08:48 +0330

    minor format and lint correction

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,7 @@ import nox
 
 venv: str = "uv|virtualenv"
 
+
 @nox.session(reuse_venv=True, venv_backend=venv)
 def install(session):
     """
@@ -28,7 +29,7 @@ def test(session):
 
 
 @nox.session(reuse_venv=True, venv_backend=venv)
-def format(session):
+def format(session):  # noqa: A001
     """
     ruff format .
     """
