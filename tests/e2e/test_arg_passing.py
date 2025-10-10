e29bcd25c3a61c5d7068b05041b5b7a5c3db2da0
Chungzter <freezycry@protonmail.com>
2025-10-10 15:06:22 +0330

    add argument passing e2e tests

---
+++ tests/e2e/test_arg_passing.py
@@ -0,0 +1,17 @@
+import subprocess
+import pytest
+
+
+@pytest.mark.parametrize(
+    "arg",
+    [
+        "-v",
+        "--version",
+        "-h",
+        "--help",
+    ],
+)
+def test_arg(arg):
+    out = subprocess.run(["commizard", arg], capture_output=True, text=True)
+    assert out.returncode == 0
+    assert out.stderr.strip() == ""
