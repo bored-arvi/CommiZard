71fa1368fbd662a18b59be9d01d9bc611d472159
Chungzter <freezycry@protonmail.com>
2025-10-15 00:46:25 +0330

    rename 'install' session to a more appropriate 'venv'

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ venv: str = "uv|virtualenv"
 
 
 @nox.session(reuse_venv=True, venv_backend=venv)
-def install(session):
+def venv(session):
     """
     Set up the development environment.
     """
